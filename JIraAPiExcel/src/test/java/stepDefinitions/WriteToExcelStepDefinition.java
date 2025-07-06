99259678fae65d936c88101341f34dd440214fa4
Saurav Singh <singh.saurav@icloud.com>
2025-07-06 12:39:49 +0530

    Modified Cucumber Version

--- JIraAPiExcel/src/test/java/stepDefinitions/WriteToExcelStepDefinition.java
+++ JIraAPiExcel/src/test/java/stepDefinitions/WriteToExcelStepDefinition.java
@@ -14,16 +14,14 @@ public class WriteToExcelStepDefinition {
     private ExcelSheetWriter excelSheetWriter;
 
     @Given("A Workbook named {string} and sheetname as{string} and Row number as {int} is read")
-    public void a_workbook_with_name_and_sub_sheet_name_and_row_number_is_read(String workbookName, String subSheetName,
-                                                                               int rowNumber) {
+    public void a_workbook_with_name_and_sub_sheet_name_and_row_number_is_read(String workbookName, String subSheetName, int rowNumber) {
         System.out.println("rowNumber from feature file is " + rowNumber);
         excelSheetReader = new ExcelSheetReader(workbookName, subSheetName, rowNumber);
         ExcelSheetManager.setExcelSheetReader(excelSheetReader);
     }
 
     @Given("A Workbook named {string} and sheetname as{string} and Row number as {int} is read and to write Data")
-    public void a_workbook_with_name_and_sub_sheet_name_and_row_number_is_readWrite(String workbookName,
-                                                                                    String subSheetName, int rowNumber) throws Exception {
+    public void a_workbook_with_name_and_sub_sheet_name_and_row_number_is_readWrite(String workbookName, String subSheetName, int rowNumber) throws Exception {
         System.out.println("rowNumber from feature file is " + rowNumber);
         excelSheetReader = new ExcelSheetReader(workbookName, subSheetName, rowNumber);
         excelSheetWriter = new ExcelSheetWriter(workbookName, subSheetName, rowNumber);
@@ -32,8 +30,7 @@ public class WriteToExcelStepDefinition {
     }
 
     @When("Read symbol as {string} and orderType as {string} and print the values")
-    public void read_symbol_as_and_order_type_as_and_print_the_values(String symbol, String orderType)
-            throws IOException {
+    public void read_symbol_as_and_order_type_as_and_print_the_values(String symbol, String orderType) throws IOException {
         if (excelSheetReader == null) {
             throw new IllegalStateException("ExcelSheetReader is not initialized for the current thread.");
         }

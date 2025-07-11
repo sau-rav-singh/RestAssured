fe845e616c140993b587064708640563c3dbbef5
Saurav Singh <singh.saurav@icloud.com>
2025-07-11 19:25:59 +0530

    Added Rest Basics

--- JIraAPiExcel/src/test/java/stepDefinitions/DefaultStepDefinition.java
+++ JIraAPiExcel/src/test/java/stepDefinitions/DefaultStepDefinition.java
@@ -21,4 +21,11 @@ public class DefaultStepDefinition {
         ExcelSheetReader reader = new ExcelSheetReader(workbookName, sheetName, rowNumber);
         ExcelSheetManager.setExcelSheetReader(reader);
     }
+
+    @Given("A Workbook named {string} and sheetname as{string} and Row number as {int} is read")
+    public void a_workbook_with_name_and_sub_sheet_name_and_row_number_is_read(String workbookName, String subSheetName, int rowNumber) {
+        System.out.println("rowNumber from feature file is " + rowNumber);
+        ExcelSheetReader excelSheetReader = new ExcelSheetReader(workbookName, subSheetName, rowNumber);
+        ExcelSheetManager.setExcelSheetReader(excelSheetReader);
+    }
 }
